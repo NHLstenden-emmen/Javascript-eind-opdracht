d58993a6fc514b10e4e876e15993a5af2c6e3aaa
Kevin Smulders <smulders.kevin.1999@gmail.com>
2021-10-19 12:10:52 +0200

    variable colors

---
+++ app/app.js
@@ -0,0 +1,8 @@
+// for loop door de games folder om alle games op te halen.
+
+// als een game is geselecteerd.
+import { startGame } from "../games/memory/memory.js";
+// import sheet from "../games/memory/memory.css";
+
+// als de player ready is kan die op start klikken
+startGame();
